b8f882a4bf1fbfe6803754aebd6362c0a9dbb834
TerenceBeijloos <42936949+TerenceBeijloos@users.noreply.github.com>
2021-01-29 11:51:13 +0100

    working uart

--- SDK/6.0.12.1020.2/projects/target_apps/peripheral_examples/blinky/include/user_periph_setup.h
+++ SDK/6.0.12.1020.2/projects/target_apps/peripheral_examples/blinky/include/user_periph_setup.h
@@ -50,8 +50,8 @@
     #define UART2_TX_PORT           GPIO_PORT_0
     #define UART2_TX_PIN            GPIO_PIN_6
 #else
-//    #define UART2_TX_PORT           GPIO_PORT_0
-//    #define UART2_TX_PIN            GPIO_PIN_4
+    #define UART2_TX_PORT           GPIO_PORT_0
+    #define UART2_TX_PIN            GPIO_PIN_4
 #endif
 
 // Define UART2 Settings
@@ -68,81 +68,14 @@
 /* LED configuration                                                                    */
 /****************************************************************************************/
 // Define UART2 Tx Pad
-//#if defined (__DA14531__)
-//    #define LED_PORT                GPIO_PORT_0
-//    #define LED_PIN                 GPIO_PIN_9
-//#else
-//    #define LED_PORT                GPIO_PORT_1
-//    #define LED_PIN                 GPIO_PIN_0
-//#endif
+#if defined (__DA14531__)
+    #define LED_PORT                GPIO_PORT_0
+    #define LED_PIN                 GPIO_PIN_9
+#else
+    #define LED_PORT                GPIO_PORT_1
+    #define LED_PIN                 GPIO_PIN_0
+#endif
 
-
-
-typedef enum{
-	one = 0,	
-	two,	three,
-	four,	five,	
-	six
-} NUMBER;
-
-//Order must be kept like this because numbers are linked to sides with the number of the side, see led_periph_init.
-typedef enum{
-	bottom = 0,
-	left,
-	front,
-	back,
-	right,
-	top
-} SIDES;
-
-typedef struct{
-		GPIO_PORT gpPort;
-		GPIO_PIN	gpPin;
-} GPIO_LED;
-
-typedef GPIO_LED GPIO_SIDE;
-
-typedef struct{
-	uint8_t 	u8Size;
-	GPIO_LED	LED_PATERN[3];
-} PATERN;
-
-#define LED_SWITCH 	false
-#define SIDE_SWITCH true
-
-/*
-static const GPIO_SIDE SIDE_TOP 					= {GPIO_PORT_0,GPIO_PIN_0};
-static const GPIO_SIDE SIDE_BOT 					= {GPIO_PORT_0,GPIO_PIN_3};
-static const GPIO_SIDE SIDE_LEFT 					= {GPIO_PORT_0,GPIO_PIN_7};
-static const GPIO_SIDE SIDE_RIGHT 				= {GPIO_PORT_0,GPIO_PIN_6};
-static const GPIO_SIDE SIDE_FRONT 				= {GPIO_PORT_0,GPIO_PIN_5};
-static const GPIO_SIDE SIDE_BACK 					= {GPIO_PORT_0,GPIO_PIN_4};
-*/
-
-static const GPIO_SIDE BOARD_TOP 						= {GPIO_PORT_2,GPIO_PIN_1}; // changed
-static const GPIO_SIDE BOARD_MAIN 					= {GPIO_PORT_0,GPIO_PIN_6}; // changed
-static const GPIO_SIDE BOARD_POWER 					= {GPIO_PORT_0,GPIO_PIN_2}; // changed
-static const GPIO_SIDE BOARD_THREE 					= {GPIO_PORT_1,GPIO_PIN_0}; // changed
-static const GPIO_SIDE BOARD_ONE 						= {GPIO_PORT_0,GPIO_PIN_7}; // changed
-static const GPIO_SIDE BOARD_TWO 						= {GPIO_PORT_1,GPIO_PIN_1}; // changed
-
-static const GPIO_LED LED_DIAGANOL_LTR 		= {GPIO_PORT_0,GPIO_PIN_5}; // changed
-static const GPIO_LED LED_DIAGANOL_RTL 		= {GPIO_PORT_3,GPIO_PIN_0}; // changed
-static const GPIO_LED LED_MIDDLE_PAIR 		= {GPIO_PORT_0,GPIO_PIN_4}; // changed
-static const GPIO_LED LED_MIDDLE_ONE 			= {GPIO_PORT_0,GPIO_PIN_3}; // changed
-
-static 			 PATERN 	 LED_PATERNS[6] 		__SECTION_ZERO("retention_mem_area0");
-static			 GPIO_SIDE SIDE_CONTAINER[6]	__SECTION_ZERO("retention_mem_area0");
-static			 SIDES 	 	 SIDE_OF_NUMBER[6] 	__SECTION_ZERO("retention_mem_area0");
-
-void led_periph_init(void);
-void led_periph_deinit(void);
-
-void led_turn_on(const NUMBER number,const SIDES side);
-void led_turn_off(const NUMBER number,const SIDES side);
-
-PATERN 	led_get_patern(const NUMBER number);
-SIDES 	led_get_side(const NUMBER number);
 /*
  * FUNCTION DECLARATIONS
  ****************************************************************************************
--- SDK/6.0.12.1020.2/projects/target_apps/peripheral_examples/blinky/src/user_periph_setup.c
+++ SDK/6.0.12.1020.2/projects/target_apps/peripheral_examples/blinky/src/user_periph_setup.c
@@ -40,130 +40,37 @@
 #include "gpio.h"
 #include "uart.h"
 
-#include "dice_led_driver.h"
-//#include "dice_communication.h"
-#include "gpio.h"
-
-
-void led_periph_init(void){
-	
-	SIDE_CONTAINER[top] 		= BOARD_TOP;
-	SIDE_CONTAINER[bottom] 	= BOARD_MAIN;
-	SIDE_CONTAINER[left] 		= BOARD_POWER;
-	SIDE_CONTAINER[right] 	= BOARD_THREE;
-	SIDE_CONTAINER[front] 	= BOARD_ONE;
-	SIDE_CONTAINER[back] 		= BOARD_TWO;
-	
-	LED_PATERNS[one].LED_PATERN[0] 		= LED_MIDDLE_ONE;
-	LED_PATERNS[one].u8Size 					= 1;
-	
-	LED_PATERNS[two].LED_PATERN[0] 		= LED_DIAGANOL_RTL;
-	LED_PATERNS[two].u8Size 					= 1;
-	
-	LED_PATERNS[three].LED_PATERN[0] 	= LED_DIAGANOL_RTL;
-	LED_PATERNS[three].LED_PATERN[1] 	= LED_MIDDLE_ONE;
-	LED_PATERNS[three].u8Size 				= 2;
-	
-	LED_PATERNS[four].LED_PATERN[0]  	= LED_DIAGANOL_RTL;
-	LED_PATERNS[four].LED_PATERN[1]  	= LED_DIAGANOL_LTR;
-	LED_PATERNS[four].u8Size 					= 2;
-	
-	LED_PATERNS[five].LED_PATERN[0]  	= LED_DIAGANOL_RTL;
-	LED_PATERNS[five].LED_PATERN[1]  	= LED_DIAGANOL_LTR;
-	LED_PATERNS[five].LED_PATERN[2]  	= LED_MIDDLE_ONE;
-	LED_PATERNS[five].u8Size 					= 3;
-	
-	LED_PATERNS[six].LED_PATERN[0] 		= LED_DIAGANOL_RTL;
-	LED_PATERNS[six].LED_PATERN[1] 		= LED_DIAGANOL_LTR;
-	LED_PATERNS[six].LED_PATERN[2] 		= LED_MIDDLE_PAIR;
-	LED_PATERNS[six].u8Size 					= 3;
-	
-	for (uint8_t i = 0; i<6; i++){
-		GPIO_PORT port 	= SIDE_CONTAINER[i].gpPort;
-		GPIO_PIN 	pin 	= SIDE_CONTAINER[i].gpPin;
-		RESERVE_GPIO(side,port,pin,PID_GPIO);
-		GPIO_ConfigurePin(port,pin, OUTPUT, PID_GPIO, !SIDE_SWITCH);
-		SIDE_OF_NUMBER[i] = (SIDES)i;
-	}
-	
-	RESERVE_GPIO(LED,LED_MIDDLE_ONE.gpPort,LED_MIDDLE_ONE.gpPin,PID_GPIO);
-	GPIO_ConfigurePin(LED_MIDDLE_ONE.gpPort,LED_MIDDLE_ONE.gpPin,OUTPUT,PID_GPIO, !LED_SWITCH);
-	
-	for (uint8_t i = 0; i<LED_PATERNS[six].u8Size; i++){
-		GPIO_PORT port = LED_PATERNS[six].LED_PATERN[i].gpPort;
-		GPIO_PIN pin 	 = LED_PATERNS[six].LED_PATERN[i].gpPin;
-		RESERVE_GPIO(led,port, pin,PID_GPIO);
-		GPIO_ConfigurePin(port, pin, OUTPUT, PID_GPIO, !LED_SWITCH);
-	}
-
-}
-
-
-void led_turn_on(const NUMBER number,const SIDES side){
-	
-	if(SIDE_SWITCH){
-		GPIO_SetActive(SIDE_CONTAINER[side].gpPort,SIDE_CONTAINER[side].gpPin);
-	}else{
-		GPIO_SetInactive(SIDE_CONTAINER[side].gpPort,SIDE_CONTAINER[side].gpPin);
-	}
-	
-	for(uint8_t i = 0; i<LED_PATERNS[number].u8Size; i++){
-		if(LED_SWITCH){
-			GPIO_SetActive(LED_PATERNS[number].LED_PATERN[i].gpPort,LED_PATERNS[number].LED_PATERN[i].gpPin);
-		}else{
-			GPIO_SetInactive(LED_PATERNS[number].LED_PATERN[i].gpPort,LED_PATERNS[number].LED_PATERN[i].gpPin);
-		}
-	}
-	
-}
-
-void led_turn_off(const NUMBER number,const SIDES side){
-	
-	if(SIDE_SWITCH){
-		GPIO_SetInactive(SIDE_CONTAINER[side].gpPort,SIDE_CONTAINER[side].gpPin);
-	}else{
-		GPIO_SetActive(SIDE_CONTAINER[side].gpPort,SIDE_CONTAINER[side].gpPin);
-	}
-	
-	for(uint8_t i = 0; i<LED_PATERNS[number].u8Size; i++){
-		if(LED_SWITCH){
-			GPIO_SetInactive(LED_PATERNS[number].LED_PATERN[i].gpPort,LED_PATERNS[number].LED_PATERN[i].gpPin);
-		}else{
-			GPIO_SetActive(LED_PATERNS[number].LED_PATERN[i].gpPort,LED_PATERNS[number].LED_PATERN[i].gpPin);
-		}
-	}
-	
-}
-
-PATERN led_get_patern(const NUMBER number){
-	return LED_PATERNS[number];
-}
-
-SIDES led_get_side(const NUMBER number){
-	return SIDE_OF_NUMBER[number];
-}
-
 static void set_pad_functions(void)
 {
 /*
     i.e. to set P0_1 as Generic purpose Output:
     GPIO_ConfigurePin(GPIO_PORT_0, GPIO_PIN_1, OUTPUT, PID_GPIO, false);
 */
-	
+
+#if defined (__DA14586__)
+    // Disallow spontaneous DA14586 SPI Flash wake-up
+    GPIO_ConfigurePin(GPIO_PORT_2, GPIO_PIN_3, OUTPUT, PID_GPIO, true);
+#endif
+
+    // Configure UART2 pin functionality
+    GPIO_ConfigurePin(UART2_TX_PORT, UART2_TX_PIN, OUTPUT, PID_UART2_TX, false);
+
+    // Configure LED pin functionality
+    GPIO_ConfigurePin(LED_PORT, LED_PIN, OUTPUT, PID_GPIO, false);
+}
 
 // Configuration struct for UART2
-//static const uart_cfg_t uart_cfg = {
-//    .baud_rate = UART2_BAUDRATE,
-//    .data_bits = UART2_DATABITS,
-//    .parity = UART2_PARITY,
-//    .stop_bits = UART2_STOPBITS,
-//    .auto_flow_control = UART2_AFCE,
-//    .use_fifo = UART2_FIFO,
-//    .tx_fifo_tr_lvl = UART2_TX_FIFO_LEVEL,
-//    .rx_fifo_tr_lvl = UART2_RX_FIFO_LEVEL,
-//    .intr_priority = 2,
-//};
-}
+static const uart_cfg_t uart_cfg = {
+    .baud_rate = UART2_BAUDRATE,
+    .data_bits = UART2_DATABITS,
+    .parity = UART2_PARITY,
+    .stop_bits = UART2_STOPBITS,
+    .auto_flow_control = UART2_AFCE,
+    .use_fifo = UART2_FIFO,
+    .tx_fifo_tr_lvl = UART2_TX_FIFO_LEVEL,
+    .rx_fifo_tr_lvl = UART2_RX_FIFO_LEVEL,
+    .intr_priority = 2,
+};
 
 void periph_init(void)
 {
@@ -175,15 +82,8 @@ void periph_init(void)
 #endif
 
     // Initialize UART2
-//    uart_initialize(UART2, &uart_cfg);
+    uart_initialize(UART2, &uart_cfg);
 
-		RESERVE_GPIO(idk1,BOARD_TWO.gpPort,BOARD_TWO.gpPin,PID_GPIO);
-		GPIO_ConfigurePin(BOARD_TWO.gpPort,BOARD_TWO.gpPin, OUTPUT, PID_GPIO, true);
-	
-		RESERVE_GPIO(led,LED_MIDDLE_ONE.gpPort, LED_MIDDLE_ONE.gpPin,PID_GPIO);
-		GPIO_ConfigurePin(LED_MIDDLE_ONE.gpPort, LED_MIDDLE_ONE.gpPin, OUTPUT, PID_GPIO, false);
-  
-	
     // Set pad functionality
     set_pad_functions();
 
